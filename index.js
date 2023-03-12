6cc032a67c1155ac053f69a232a40f0b30e598a7
KeithPetr <keith.petryshyn@gmail.com>
2023-03-12 12:12:10 -0400

    Basic functionality complete

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const colorEl = document.getElementById('color-input')
 const schemeEl = document.getElementById('scheme-input')
 const getColorBtn = document.getElementById('get-color-btn')
+const mainEl = document.getElementById('main')
+
+let html = ''
 
 getColorBtn.addEventListener('click', (e) => {
-    console.log(colorEl.value)
-    console.log(schemeEl.value)
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorEl.value.substring(1)}&mode=${schemeEl.value}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        html = data.colors.map( color => {
+            return `
+            <div class="color-container">
+                <div class="color"><img src="${color.image.bare}"></div>
+                <p class="hex-value" id="hex-value">${color.hex.value}</p>
+            </div>
+            `
+        }).join('')
+        mainEl.innerHTML = html
+    })
 })
 
 
