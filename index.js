3007af25a8730839481c61757643b9ecec1d8798
KeithPetr <keith.petryshyn@gmail.com>
2023-03-12 12:56:42 -0400

    Added copy function of hex values

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const colorEl = document.getElementById('color-input')
-const schemeEl = document.getElementById('scheme-input')
-const getColorBtn = document.getElementById('get-color-btn')
-const mainEl = document.getElementById('main')
+const colorEl = document.getElementById("color-input");
+const schemeEl = document.getElementById("scheme-input");
+const getColorBtn = document.getElementById("get-color-btn");
+const mainEl = document.getElementById("main");
 
-let html = ''
+let html = "";
 
-getColorBtn.addEventListener('click', (e) => {
-    fetch(`https://www.thecolorapi.com/scheme?hex=${colorEl.value.substring(1)}&mode=${schemeEl.value}`)
-    .then(res => res.json())
-    .then(data => {
-        html = data.colors.map( color => {
-            return `
+getColorBtn.addEventListener("click", (e) => {
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${colorEl.value.substring(1)}&mode=${schemeEl.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      html = data.colors
+        .map((color) => {
+          return `
             <div class="color-container">
                 <div class="color"><img src="${color.image.bare}"></div>
                 <p class="hex-value" id="hex-value">${color.hex.value}</p>
             </div>
-            `
-        }).join('')
-        mainEl.innerHTML = html
-    })
-})
+            `;
+        })
+        .join("");
+      mainEl.innerHTML = html;
+    });
+});
 
+function copyHexValue(e) {
+    const hexValueEl = e.target;
+    const hexValueText = hexValueEl.textContent;
+    const textarea = document.createElement("textarea");
+    textarea.value = hexValueText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  
+    // Show alert with hex value
+    alert(`Copied ${hexValueText} to clipboard!`);
+  }
 
-
+mainEl.addEventListener("click", (e) => {
+  if (e.target.matches("#hex-value")) {
+    copyHexValue(e);
+  }
+});
